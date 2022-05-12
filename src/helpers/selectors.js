493edcfc8d6e4b28e008459a1aefa3c199714ece
Shraddha-Naidu <naidu.shraddha@gmail.com>
2022-05-11 20:20:01 -0600

    Add working helpers

---
+++ src/helpers/selectors.js
@@ -0,0 +1,34 @@
+export function getAppointmentsForDay(state, day) {
+  const appArr = [];
+  state.days.forEach((dayOf) => {
+    if (dayOf.name === day){
+      dayOf.appointments.forEach((event) => {
+        appArr.push(state.appointments[event]);
+      });
+    }
+  });
+  return appArr;
+}
+
+export function getInterviewersForDay(state, day) {
+  const intArr = [];
+  state.days.forEach((dayOf) => {
+    if (dayOf.name === day){
+      dayOf.interviewers.forEach((event) => {
+        intArr.push(state.interviewers[event]);
+      });
+    }
+  });
+  return intArr;
+}
+
+export function getInterview(state, interview) {
+  if (!interview) {
+    return null;
+  }
+  let interviewObj = {};
+  interviewObj.student = interview.student;
+  interviewObj.interviewer = state.interviewers[interview.interviewer];
+
+  return interviewObj;
+}
